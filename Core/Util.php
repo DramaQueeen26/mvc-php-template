84e61b97c025958a907e810fa49415cc0069a679
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-05-19 14:16:58 -0430

    Agregando sweet alert y clean string a Util.php

--- Core/Util.php
+++ Core/Util.php
@@ -12,4 +12,74 @@ class Util
         $baseUrl = str_replace(PUBLIC_FOLDER.'/', '', $baseUrl);
         return $baseUrl;
     }
+    public function cleanString($string)
+    {
+        $string = trim($string);
+        $string = stripslashes($string);
+        $string = htmlentities($string);
+        $string = addslashes($string);
+        return $string;
+    }
+    public function sweetAlert($data)
+    {
+        if($data['alert'] == "simple"){
+            $alert = " 
+                <script>
+                    swal({
+                        type: '".$data['type']."',
+                        title: '".$data['title']."',
+                        text: '".$data['text']."'
+                    });
+                </script>
+            ";
+
+        }elseif($data['alert'] == "reload") {
+
+            $alert = " 
+                <script>
+                    swal({
+                        type: '".$data['type']."',
+                        title: '".$data['title']."',
+                        text: '".$data['text']."',
+                        confirmButtonText: 'OK'
+                        }).then(function(){
+                                location.reload();
+                        });
+                </script>
+            ";
+
+        }elseif($data['alert'] == "clean") {
+
+            $alert = " 
+                <script>
+                    swal({
+                        type: '".$data['type']."',
+                        title: '".$data['title']."',
+                        text: '".$data['text']."',
+                        confirmButtonText: 'OK'
+                        }).then(function(){
+                                $('form')[0].reset();
+                        });
+                </script>
+            ";
+
+        }elseif($data['alert'] == "redirect") {
+
+            $alert = " 
+                <script>
+                    swal({
+                        type: '".$data['type']."',
+                        title: '".$data['title']."',
+                        text: '".$data['text']."',
+                        confirmButtonText: 'OK'
+                        }).then(function(){
+                                window.location.href = '".self::baseUrl()."'
+                        });
+                </script>
+            ";
+
+        }
+
+        return $alert;
+    }
 }
